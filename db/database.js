07e93d21d26c58edd3f7c8735c644c6fab1354fe
Fabian Narvaez <narvaezfb4@hotmail.com>
2022-01-07 22:44:07 -0500

    file changsed

--- db/database.js
+++ db/database.js
@@ -1,22 +1,23 @@
 const Pool = require("pg").Pool;
 
-// const pool = new Pool({
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   host: process.env.HOST,
-//   port: process.env.DB_PORT,
-//   database: process.env.DB_NAME,
-// });
+const pool = new Pool({
+  user: "fabian",
+  password: "CNSA2020",
+  host: "/cloudsql/cnsadb",
+  // port: process.env.DB_PORT,
+  database: "cnsadb",
+  connector: "postgresql",
+});
 
-const pool = {
-  yourdbname: {
-    host: "/cloudsql/cnsadb",
-    database: "cnsadb", // Create at step 3
-    password: "CNSA2020",
-    user: "fabian",
-    name: "cnsadb",
-    connector: "postgresql",
-  },
-};
+// const pool = {
+//   yourdbname: {
+//     host: "/cloudsql/cnsadb",
+//     database: "cnsadb", // Create at step 3
+//     password: "CNSA2020",
+//     user: "fabian",
+//     name: "yourdbname",
+//     connector: "postgresql",
+//   },
+// };
 
 module.exports = pool;
