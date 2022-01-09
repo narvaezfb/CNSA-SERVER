ed503c3a3d58629860de054b87ce5a8fae6003b0
Fabian Narvaez <narvaezfb4@hotmail.com>
2022-01-08 19:22:36 -0500

    commit from vscode

--- app.js
+++ app.js
@@ -25,14 +25,6 @@ const totalGamesRouter = require("./Routes/totalGamesRoutes");
 const gameStatistics = require("./Routes/gameStatistics");
 const playerStatisticsRouter = require("./Routes/playerStatisticsRoutes");
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
